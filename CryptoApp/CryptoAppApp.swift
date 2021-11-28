6877bb58b013e3efbd49fe750d2898b84dd78458
Douglas Santos <douglas.gsantos@icloud.com>
2021-11-28 11:46:10 -0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Douglas Santos on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
